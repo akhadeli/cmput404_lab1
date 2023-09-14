b5b9524df26d9d6c7e983a8e3d5448db6d44d16c
Abdullah Khadeli <akhadeli@cs.ualberta.ca>
2023-09-13 18:01:27 -0600

    changes

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ import requests
 
 print(requests.__version__)
 requests.get('http://google.com/')
+resp = requests.get('https://raw.githubusercontent.com/akhadeli/cmput404_lab1/main/script.py')
+print(resp.text)
